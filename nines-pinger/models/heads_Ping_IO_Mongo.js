2be649418a0ca9088d750dc71cdb5813dfea5170
Matt <mattlam@uw.edu>
2016-01-22 09:36:46 -0800

    New URL ping log to Heads model now includes incement of error and response totals for URL on Urls model

--- nines-pinger/models/heads_Ping_IO_Mongo.js
+++ nines-pinger/models/heads_Ping_IO_Mongo.js
@@ -16,41 +16,48 @@ var errorThreshold = 400;
  */
 
 // Create new MongoDB doc in Heads collection for a response to HEAD request
-// Create new MongoDB doc in Heads collection for a response to HEAD request
+// for this URL. Also update the Urls collection to appropriately increment the
+// response and error totals for this URL.
 exports.writeHeadsEntry = function(reqDateTime, urlID, statusCode) {
-    // Create a new general log entry from data passed to this function
+    // Create a new Heads entry from data passed to this function
     var newHeadsEntry = Heads({
         datetime: reqDateTime,
         url_id: urlID,
         status_code: statusCode
     });
 
-    // Save the new Heads entry to MongoDB
-    newHeadsEntry.save(function(err, heads) {
+    // Save the new Heads entry to MongoDB and update the Urls model to
+    // appropriately increment the response and error totals for this URL
+    newHeadsEntry.save(function(err, head) {
         if (err) console.log(err);
         console.log('HEAD request logged');
-        Urls.find({ '_id': heads.url_id }, 'response_total error_total', function(err, urls) {
-            if (err) console.log(err);
-            console.log('response total:', urls.response_total);
-            console.log('error total:', urls.error_total);
-        });
-        /*
-        Heads.find(function(err, heads) {
-            if (err) console.log(err);
-            if (heads) {
-                var results = [];
-                var errTotal = 0;
-                var resTotal = 0;
-                for (var i = 0; i < heads.length; i++) {
-                    if (heads[i].url_id == urlID) {
-                        if (heads[i].status_code >= errorThreshold) {
-                            errTotal += 1;
-                        }
-                        resTotal += 1;
-                    }
+
+        // Get the Urls model doc for this URL
+        Urls.findOne(
+            { '_id': head.url_id },
+            function(err, url) {
+                if (err) console.log(err);
+
+                // If the status code for the HEAD request response was equal
+                // to or greater than the error threshold, then the error
+                // total for this URL will need to be incremented by 1
+                var errIncrement = 0;
+                if (head.status_code >= errorThreshold) {
+                    errIncrement = 1;
                 }
+
+                // Increment the response and error totals for URL
+                Urls.update(
+                    { '_id': head.url_id },
+                    {
+                        'response_total': url.response_total + 1,
+                        'error_total': url.error_total + errIncrement
+                    },
+                    function(err, numAffected) {
+                        if (err) console.log(err);
+                    }
+                );
             }
-        });
-        */
+        );
     });
 };
